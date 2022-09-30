1935eab8f64f0377fd385920bda139941d15d5da
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-09-30 13:00:00 +0100

    Count the frequencies of items in a list
    All of the items in the list are converted to String objects and then the occurrence of each item is added to a dictionary.

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    # Convert everything to strings
+    for i in range(0, len(items)):
+        items[i] = str(items[i])
+
+
+    # Count the frequencies
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        if item not in frequencies.keys():
+            frequencies[item] = items.count(item)
+
     return frequencies
